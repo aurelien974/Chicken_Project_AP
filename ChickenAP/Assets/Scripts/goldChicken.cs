fabe74a8be86172a3249739c37e245d4967940bc
Aurélien Picolet <aurelien.picolet@gmail.com>
2015-02-03 15:00:03 +0100

    3rd commit
    with github for windows

---
+++ ChickenAP/Assets/Scripts/goldChicken.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goldChicken : MonoBehaviour {
+
+	private int numberOfGoldChickenFound;
+
+	private particlesGeneration particlesScript;
+	public ParticleSystem particles;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		particlesScript = transform.GetComponent<particlesGeneration>();
+
+		numberOfGoldChickenFound = 0;
+		particles.startColor = particlesScript.colors[numberOfGoldChickenFound];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col) {
+
+		if(col.gameObject.tag == "goldChicken") {
+
+			numberOfGoldChickenFound++;
+			//col.gameObject.animation;
+			col.gameObject.collider.enabled = false;
+			Debug.Log(numberOfGoldChickenFound);
+
+
+		}
+
+	}
+}
---
+++ ChickenAP/Assets/Scripts/particlesGeneration.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particlesGeneration : MonoBehaviour {
+
+	ballMovement ballMvt;
+
+	public float emissionRateMin = 2.0f;
+	public float emissionRateMax = 20.0f;
+	public float emissionStartSpeedMin = 1.0f;
+	public float emissionStartSpeedMax = 6.0f;
+
+	public ParticleSystem particles;
+
+	public Color[] colors = new Color[6];
+
+	// Use this for initialization
+	void Start () {
+	
+		ballMvt = transform.GetComponent<ballMovement>();
+
+		//colors[0] = new Color(109,79,0);
+		//colors[1] = Color.red;
+		//colors[2] = Color.yellow;
+		//colors[3] = new Color(255,130,0);
+		//colors[4] = Color.blue;
+		//colors[5] = Color.magenta;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if(ballMvt.IsGrounded() && rigidbody.velocity.magnitude > 1) {
+
+			particles.enableEmission = true;
+			particles.emissionRate = emissionRateMin + (emissionRateMax - emissionRateMin) * rigidbody.velocity.magnitude / ballMvt.maxSpeed;
+			particles.startSpeed = emissionStartSpeedMin + (emissionStartSpeedMax - emissionStartSpeedMin) * rigidbody.velocity.magnitude / ballMvt.maxSpeed;
+			
+		} else {
+
+			particles.enableEmission = false;
+
+		}
+
+	}
+}
