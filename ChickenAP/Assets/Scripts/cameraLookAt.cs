c738b10a7aaf466c6bb1d18d2556a8540b2743e5
Aurélien Picolet <aurelien.picolet@gmail.com>
2015-03-10 13:36:20 +0100

    cameras modified to do the game trailer game trailer added

---
+++ ChickenAP/Assets/Scripts/cameraLookAt.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraLookAt : MonoBehaviour {
+
+	public Transform target;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.LookAt(target);
+
+	}
+}
